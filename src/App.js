fa914603c069313befde3c38d591f082ee331d11
Tumaini8 <lutufyotumaini6@gmail.com>
2021-10-23 15:49:36 +0300

    initial commit and added a search engine

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import Logo from "./Logo.jpg";
+import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={Logo} className="App-logo" alt="logo" />
       </header>
+      <main>
+        <Dictionary />
+        <footer className="App-footer">
+          <em>
+            {" "}
+            <p className="mt-4 text-success">Coded by Tumaini Edgar</p>
+          </em>
+        </footer>
+      </main>
     </div>
   );
 }
